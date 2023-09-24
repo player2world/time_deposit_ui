7a4580b9fd8f70f56166ada2c0ab8eb8085e2a26
Justin Lee <justinleeyile@gmail.com>
2023-09-24 15:54:00 +0800

    fix issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { useStoreContext } from "./utils/Store";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { connection } = useConnection();
+  const wallet = useWallet();
+  const { programClient } = useStoreContext();
 
   return (
     <>
@@ -18,9 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        Wallet is {wallet.publicKey?.toString()}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Wallet } from "./components/Wallet.tsx";
+import { StoreProvider } from "./utils/Store.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Wallet>
-      <App />
-    </Wallet>
+    <StoreProvider>
+      <Wallet>
+        <App />
+      </Wallet>
+    </StoreProvider>
   </React.StrictMode>
 );
